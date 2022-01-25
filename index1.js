124b128573e71f40230bd1a175a3091f8c0cd57a
faizan <faizanahmad72560@gmail.com>
2022-01-25 14:12:37 +0530

    initial commit

---
+++ index1.js
@@ -0,0 +1,89 @@
+console.log('welcome to tic tac toe');
+let music= new Audio('music.mp3');
+let audioTurn= new Audio('ting.mp3');
+let gameover= new Audio('gameover.mp3');
+let turn='X';
+let isgameover=false;
+
+//function to change turn
+
+function changeTurn()
+{
+    if(turn==='X')
+    turn='0';
+    else
+    turn='X';
+}
+// check win
+
+const checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e =>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+            isgameover = true
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            gameover.play();
+            setTimeout(()=>{
+                isreset();
+
+            },3000)
+            // document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            // document.querySelector(".line").style.width = "20vw";
+        }
+    })
+}
+
+//game logic
+
+music.play();
+
+let boxes=document.getElementsByClassName('box');
+Array.from(boxes).forEach(element=>{
+    let boxtext=element.querySelector('.boxtext')
+    element.addEventListener('click',()=>{
+        if(boxtext.innerText==='')
+        {
+            boxtext.innerText=turn;
+            changeTurn();
+            audioTurn.play();
+            checkWin();
+            if(!isgameover){
+                document.getElementsByClassName('info')[0].innerText='turn for'+turn;
+            }
+            
+
+            
+        }
+    })
+})
+
+
+//adding event listener to reset button
+
+let reset=document.getElementById('reset');
+reset.addEventListener('click',isreset)
+function isreset(element)
+{
+    // let boxtext=document.querySelectorAll('.boxtext');
+    // Array.from(boxtext).forEach(element =>{
+    //     element.innerText='';
+    // });
+    // turn='X';
+    // isgameover=false;
+    // document.querySelector(".line").style.width = "0vw";
+    // document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+    // document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
+    window.location.reload();
+
+}
